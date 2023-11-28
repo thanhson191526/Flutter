5e600d22f7efadc04dab7d1d045ce680ae0f8d97
LE THANH SON <leson1901@gmail.com>
2023-11-28 22:02:43 +0700

    Add Logo LoginScreen

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -10,144 +10,152 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: const Text('Ứng dụng quản lý điểm'),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                height: 40,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.login,
-                      color: Colors.white,
-                      size: 20,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: const Text('Ứng dụng quản lý điểm'),
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Icon(
+                    Icons.school,
+                    size: 150,
+                    color: Colors.black,
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    width: double.infinity,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    SizedBox(width: 10),
-                    Text(
-                      'Đăng nhập hệ thống',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.login,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          'Đăng nhập hệ thống',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  TextFormField(
+                    controller: usernameController,
+                    decoration: const InputDecoration(
+                      hintText: 'Nhập tên đăng nhập',
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      labelText: 'Tên đăng nhập',
+                      labelStyle: TextStyle(color: Colors.black),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
                       ),
                     ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 30),
-              TextFormField(
-                controller: usernameController,
-                decoration: const InputDecoration(
-                  hintText: 'Nhập tên đăng nhập',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
+                    style: const TextStyle(color: Colors.black),
                   ),
-                  labelText: 'Tên đăng nhập',
-                  labelStyle: TextStyle(color: Colors.black),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: passwordController,
+                    decoration: const InputDecoration(
+                      hintText: 'Nhập mật khẩu',
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      labelText: 'Mật khẩu',
+                      labelStyle: TextStyle(color: Colors.black),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                    ),
+                    obscureText: true,
+                    style: const TextStyle(color: Colors.black),
                   ),
-                ),
-                style: const TextStyle(color: Colors.black),
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  hintText: 'Nhập mật khẩu',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                  labelText: 'Mật khẩu',
-                  labelStyle: TextStyle(color: Colors.black),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                  ),
-                ),
-                obscureText: true,
-                style: const TextStyle(color: Colors.black),
-              ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(primary: Colors.black),
-                child: const Text(
-                  'Đăng nhập',
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () async {
-                  String enteredUsername = usernameController.text;
-                  String enteredPassword = passwordController.text;
+                  const SizedBox(height: 30),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(primary: Colors.black),
+                    child: const Text(
+                      'Đăng nhập',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () async {
+                      String enteredUsername = usernameController.text;
+                      String enteredPassword = passwordController.text;
 
-                  if (enteredUsername.isNotEmpty &&
-                      enteredPassword.isNotEmpty) {
-                    List<UserAccount> accounts =
-                        await UserDataManager.getUserData();
-                    bool isLoggedIn = accounts.any((account) =>
-                        account.username == enteredUsername &&
-                        account.password == enteredPassword);
+                      if (enteredUsername.isNotEmpty &&
+                          enteredPassword.isNotEmpty) {
+                        List<UserAccount> accounts =
+                            await UserDataManager.getUserData();
+                        bool isLoggedIn = accounts.any((account) =>
+                            account.username == enteredUsername &&
+                            account.password == enteredPassword);
 
-                    if (isLoggedIn) {
+                        if (isLoggedIn) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => HomeScreen(enteredUsername),
+                            ),
+                          );
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Đăng nhập tài khoản thành công'),
+                              duration: Duration(seconds: 2),
+                            ),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text(
+                                  'Sai tài khoản hoặc mật khẩu. Xin thử lại!'),
+                              duration: Duration(seconds: 2),
+                            ),
+                          );
+                        }
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text(
+                                'Vui lòng điền đầy đủ thông tin đăng nhập'),
+                            duration: Duration(seconds: 2),
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(primary: Colors.black),
+                    child: const Text(
+                      'Chưa có tài khoản? Đăng ký ngay',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    onPressed: () {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => HomeScreen(enteredUsername),
-                        ),
+                            builder: (context) => RegisterScreen()),
                       );
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('Đăng nhập tài khoản thành công'),
-                          duration: Duration(seconds: 2),
-                        ),
-                      );
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content:
-                              Text('Sai tài khoản hoặc mật khẩu. Xin thử lại!'),
-                          duration: Duration(seconds: 2),
-                        ),
-                      );
-                    }
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content:
-                            Text('Vui lòng điền đầy đủ thông tin đăng nhập'),
-                        duration: Duration(seconds: 2),
-                      ),
-                    );
-                  }
-                },
+                    },
+                  ),
+                ],
               ),
-              TextButton(
-                style: TextButton.styleFrom(primary: Colors.black),
-                child: const Text(
-                  'Chưa có tài khoản? Đăng ký ngay',
-                  style: TextStyle(color: Colors.black),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => RegisterScreen()),
-                  );
-                },
-              ),
-            ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
